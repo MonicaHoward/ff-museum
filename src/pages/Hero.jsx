4630f7e70661973177f30f5d9783ecc7f610af00
Monica <jay.m.howard@gmail.com>
2022-08-17 18:01:20 -0500

    added hero page and title component

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Title from '../components/Title';
+
+const Hero = () => {
+    return(
+        <>
+        <div>
+            <h1>Celebrating Women Filmmakers</h1>
+        </div>
+        </>
+    )
+}
+
+export default Hero;
