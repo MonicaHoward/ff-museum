d4d378b8be18e897536673565ccb4a8bc6a72c76
Monica <jay.m.howard@gmail.com>
2022-08-17 21:54:05 -0500

    basic Hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
-
-import Title from '../components/Title';
+import styled from 'styled-components'
 
 const Hero = () => {
     return(
-        <>
-        <div>
-            <h1>Celebrating Women Filmmakers</h1>
-        </div>
-        </>
+        <Wrapper>
+            <Title>Celebrating Women<br/> Filmmakers</Title>
+            <section>
+                <div></div>
+                <Subtitle>Women who are changing the future of film</Subtitle>
+            </section>
+           
+        </Wrapper>
     )
 }
 
 export default Hero;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    padding: 2%;
+`
+
+const Title = styled.h1`
+    font-size: 6em;
+    font-weight: 400;
+    font-family: 'Cinzel', serif;
+`
+
+const Subtitle = styled.h3`
+    font-size: 2em;
+    font-family: sans-serif;
+`
